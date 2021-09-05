d83e2b8d5d47cdaabde4e04632b88fbd33203f93
Lizwe <macginyani@gmai.com>
2021-09-05 17:11:45 +0200

    Reuploading some files

--- app/src/main/java/za/co/gundula/navdrawer/MainActivity.kt
+++ app/src/main/java/za/co/gundula/navdrawer/MainActivity.kt
@@ -1,7 +1,9 @@
 package za.co.gundula.navdrawer
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.widget.Button
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -37,11 +39,29 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_reports, R.id.nav_privacy, R.id.nav_bugs
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        val button1 = findViewById<Button>(R.id.button1)
+        button1.setOnClickListener {
+            val intent = Intent(this, ReportOneActivity::class.java)
+            startActivity(intent)
+        }
+
+        val button2 = findViewById<Button>(R.id.button2)
+        button2.setOnClickListener {
+            val intent = Intent(this, ReportTwoActivity::class.java)
+            startActivity(intent)
+        }
+
+        val button3 = findViewById<Button>(R.id.button3)
+        button3.setOnClickListener {
+            val intent = Intent(this, ReportThreeActivitty::class.java)
+            startActivity(intent)
+        }
 
     }
 
